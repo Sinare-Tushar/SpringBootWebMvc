8398d04875e4f52b246b15c5ef1521ff97e6d758
Sinare-Tushar <tusharsinare020295@gmail.com>
2023-05-23 15:43:28 +0530

    add requestmethod

--- src/main/java/com/app/controller/EmployeeController.java
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -5,13 +5,14 @@ import java.util.Date;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
    @Controller
 @RequestMapping(path = "emp")
 public class EmployeeController {
-	@RequestMapping(path = "/show")
+	@PostMapping(path = "/show")
 	public String showPages(Model m) {
 		m.addAttribute("msg","Welcome App:" + new Date());
 		return "Home";
